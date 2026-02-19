6b6d8472c283288317b265e70447d42f195ba599
snmuratova <snmuratova@gmail.com>
2026-02-19 12:10:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Бот работает 🚀")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
