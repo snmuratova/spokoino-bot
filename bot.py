3f4e186a7a4174aa02ff838ca2453caf6d3db999
snmuratova <snmuratova@gmail.com>
2026-02-19 14:53:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import asyncio
+import os
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
-import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -13,8 +14,23 @@ dp = Dispatcher()
 async def start(message: Message):
     await message.answer("Бот работает 🚀")
 
-async def main():
+async def start_bot():
     await dp.start_polling(bot)
+
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+async def start_web():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    port = int(os.environ.get("PORT", 10000))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
+async def main():
+    await asyncio.gather(start_bot(), start_web())
 
 if __name__ == "__main__":
     asyncio.run(main())
