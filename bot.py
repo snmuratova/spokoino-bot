892a41f3d96ca2918c3e9dfd07749da5ea32fddd
snmuratova <snmuratova@gmail.com>
2026-02-20 23:26:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ def kb_anxiety_scale():
     kb = InlineKeyboardBuilder()
     
     for i in range(0, 11):
-    kb.button(text=str(i), callback_data=f"anxiety:{i}")
+        kb.button(text=str(i), callback_data=f"anxiety:{i}")
     
     kb.adjust(6, 5)  # 0–5 в строке, 6–10 ниже
     kb.button(text="🏠 Меню", callback_data="menu")
